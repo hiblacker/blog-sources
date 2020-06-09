9fd4d6fd9e032c803e8da3bbbea9743954e53dd3
hiblacker <25556621@qq.com>
2020-06-09 21:16:28 +0800

    promise test 增加备注

--- practice/javascript/promise/promiseTest.js
+++ practice/javascript/promise/promiseTest.js
@@ -19,3 +19,7 @@ new Promise((resolve, reject) => {
     .catch(() => console.log(7))
     .then(() => console.log(8));
 
+
+// 注：根据规范 [2.2.4](https://promisesaplus.com/#point-34) then 中 onFulfilled 或 onRejected 必须在执行环境堆栈只包含平台代码后调用。
+
+// 平台代码：引擎，环境，和 promise 实现代码。实际上，这个要求确保 onFulfilled 和 onRejected 都在下一轮的事件循环中（一个新的栈）被异步调用。可以用宏任务，例如：setTimeout，setImmediate 或者微任务，例如：MutationObsever 或 process.nextTick 实现。 由于 promise 的实现被当做平台代码，所以它本身可能包含一个任务队列或 “trampoline” 的处理程序
